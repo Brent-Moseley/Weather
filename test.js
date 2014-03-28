e58c6f08446242691c9860332c89156359498a96
Brent-Moseley <techwrite88@gmail.com>
2014-03-27 19:14:55 -0700

    Completed.

--- test.js
+++ test.js
@@ -1,28 +1,37 @@
 // Solution to Kata 4, part I:  http://codekata.com/kata/kata04-data-munging/
 
 
-fs = require('fs');
+// Initialize some useful stuff:
+var fs = require('fs');    // Node file IO lib, http://nodejs.org/api/fs.html
 var data, lines;
 var ready = false;
+var smallest = 9999;
+var num = 0;
+
+// Read in data file, calculating results when read. 
 fs.readFile('weather.dat', 'utf8', function (err,data) {
   if (err) {
     return console.log(err);
   }
   lines = data.split('\n');
-  //console.log(data);
-  ready = true;
   show();
+  return;
 });
 
 function show () {
-  console.log('Lines read: ' + lines.length);
-  for (var i = 2; i < lines.length; i++) {    // skip header and blank line
-  	//console.log (data2[i]);
-  	console.log ("Line: " + i);
-  	var vals = lines[i].split(' ');
-  	console.log (vals[0] + ',' + vals[1] + ',' + vals[2]);
-  	// for (var j = 0; j < 3; j++) {
-  		
-  	// } 
+  console.log('\nLines read from data file: ' + lines.length);
+  for (var i = 2; i < lines.length-1; i++) {    // Loop through all lines, skipping header and blank line
+  	var vals = lines[i].trim().split(/\s+/);    // divide fields on one or more spaces
+    high = vals[1].replace (/[!@#$%^&*]/g, ''); // get high temp, cleaning any strange chars
+    low = vals[2].replace (/[!@#$%^&*]/g, '');  // get low temp
+
+  	//console.log (vals[0] + ',' + high + ',' + low);
+    var diff = parseInt(high) - parseInt(low);
+    if (diff < smallest) {
+      smallest = diff;     // record new smallest difference
+      num = vals[0];       // record line number
+    }
+
   }
+  console.log ("Lowest spread was day " + num + " with a temp diff of " + smallest);
 }  

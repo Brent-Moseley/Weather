33846159b731aa06b4a2985f4134713d4f264ad1
Brent-Moseley <techwrite88@gmail.com>
2014-03-27 21:44:56 -0700

    Added largest temp spread.

--- test.js
+++ test.js
@@ -5,8 +5,8 @@
 var fs = require('fs');    // Node file IO lib, http://nodejs.org/api/fs.html
 var data, lines;
 var ready = false;
-var smallest = 9999;
-var num = 0;
+var smallest = 9999, largest = 0;
+var num_small = num_large = 0;
 
 // Read in data file, calculating results when read. 
 fs.readFile('weather.dat', 'utf8', function (err,data) {
@@ -22,16 +22,21 @@ function show () {
   console.log('\nLines read from data file: ' + lines.length);
   for (var i = 2; i < lines.length-1; i++) {    // Loop through all lines, skipping header and blank line
     var vals = lines[i].trim().split(/\s+/);    // divide fields on one or more spaces
-    high = vals[1].replace (/[!@#$%^&*]/g, ''); // get high temp, cleaning any strange chars
-    low = vals[2].replace (/[!@#$%^&*]/g, '');  // get low temp
+    var high = vals[1].replace (/[!@#$%^&*]/g, ''); // get high temp, cleaning any strange chars
+    var low = vals[2].replace (/[!@#$%^&*]/g, '');  // get low temp
 
     //console.log (vals[0] + ',' + high + ',' + low);
     var diff = parseInt(high) - parseInt(low);
     if (diff < smallest) {
       smallest = diff;     // record new smallest difference
-      num = vals[0];       // record line number
+      num_small = vals[0];       // record line number
+    }
+    if (diff > largest) {
+      largest = diff;
+      num_large = vals[0];
     }
 
   }
-  console.log ("Lowest spread was day " + num + " with a temp diff of " + smallest);
+  console.log ("Lowest spread was day " + num_small + " with a temp diff of " + smallest);
+  console.log ("Highest spread was day " + num_large + " with a temp diff of " + largest);
 }  
